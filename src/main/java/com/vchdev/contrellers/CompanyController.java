53ec5215d402286d42156bec4032fd7efdbf5e25
Вячеслав <viacheslav.cgernyavskij@gmail.com>
2024-07-29 01:20:01 +0300

    implementing Response Status

--- src/main/java/com/vchdev/contrellers/CompanyController.java
+++ src/main/java/com/vchdev/contrellers/CompanyController.java
@@ -7,9 +7,13 @@ import com.vchdev.services.CompanyService;
 import com.vchdev.util.EntityConverter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Slf4j
@@ -29,7 +33,26 @@ public class CompanyController {
     }
 
     @PostMapping("/add")
-    public void addCompany(@RequestBody CompanyTO companyTO){
+    public ResponseEntity addCompany(@RequestBody CompanyTO companyTO){
+        return new ResponseEntity<>(EntityConverter.convertToDto(service.saveOrUpdate((Company) EntityConverter.convertToEntity(companyTO))), HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity updateCompany(@RequestBody CompanyTO companyTO){
         service.saveOrUpdate((Company) EntityConverter.convertToEntity(companyTO));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity deleteCompany(@RequestBody Long id){
+        service.delete(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/show")
+    public ResponseEntity<?> findCompany(@RequestBody Long id){
+        Optional<Company> company = service.findById(id);
+        return company.map(value -> ResponseEntity.ok(EntityConverter.convertToDto(value))).
+                orElseGet(() -> new ResponseEntity("Company not found.", HttpStatus.NOT_ACCEPTABLE));
     }
 }
